71d7a8859d82ea64e520ebc93e4d5a4bef45629f
Axel Baudot <axel.baudot@protonmail.com>
2024-09-03 18:47:39 -0400

    Add Maximize Oracle constraint

---
+++ src/main/java/minicpbp/engine/constraints/MaximizeOracle.java
@@ -0,0 +1,68 @@
+/*
+ * mini-cp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License  v3
+ * as published by the Free Software Foundation.
+ *
+ * mini-cp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU Lesser General Public License  for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with mini-cp. If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
+ *
+ * Copyright (c)  2018. by Laurent Michel, Pierre Schaus, Pascal Van Hentenryck
+ *
+ * mini-cpbp, replacing classic propagation by belief propagation
+ * Copyright (c)  2019. by Gilles Pesant
+ */
+
+package minicpbp.engine.constraints;
+
+import minicpbp.engine.core.AbstractConstraint;
+import minicpbp.engine.core.IntVar;
+
+/**
+ * Oracle unary constraint providing fixed marginals (possibly through ML)
+ * Does not perform any filtering
+ */
+public class MaximizeOracle extends AbstractConstraint {
+    private IntVar x;
+
+    /**
+     * @param x the variable
+     *          Note: any domain value not appearing in v will be assigned a zero marginal
+     */
+    public MaximizeOracle(IntVar x) {
+        super(x.getSolver(), new IntVar[]{x});
+        setName("Max Oracle");
+        this.x = x;
+        setExactWCounting(true);
+    }
+
+    @Override
+    public void post() {
+    }
+
+    @Override
+    public void propagate() {
+    }
+
+    @Override
+    public void updateBelief() {
+        float sum = 0;
+        int L = x.min();
+        int U = x.max();
+        for (int val = L; val <= U; val++) {
+            if (x.contains(val)) {
+                sum += ((float) (val - L + 1)) / ((float) (Math.abs(val - L) + U - L + 1));
+                // val = L -> b = 1 / (U - L + 1)
+                // val = U -> b = (U - L + 1) / 1
+            }
+        }
+        for (int val = L; val <= U; val++) {
+            if (x.contains(val)) {
+                setLocalBelief(0, val, val / sum);
+            }
+        }
+    }
+}
---
+++ src/test/java/minicpbp/engine/constraints/MaximizeOracleTest.java
@@ -0,0 +1,23 @@
+package minicpbp.engine.constraints;
+
+import minicpbp.engine.core.IntVar;
+import minicpbp.engine.core.Solver;
+import org.junit.Test;
+
+import static minicpbp.cp.Factory.makeIntVar;
+import static minicpbp.cp.Factory.makeSolver;
+
+public class MaximizeOracleTest {
+
+    @Test
+    public void maximizeTest() {
+        Solver cp = makeSolver();
+        IntVar x = makeIntVar(cp, 10, 20);
+
+        cp.setTraceBPFlag(true);
+        cp.post(new MaximizeOracle(x));
+        cp.vanillaBP(10);
+    }
+
+
+}
